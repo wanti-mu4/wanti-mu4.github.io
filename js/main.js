121fe2308a0711771dad8bff28f9d5deede7dc7d
wanti-mu4 <94421220+wanti-mu4@users.noreply.github.com>
2022-07-18 18:27:33 +0900

    プログラミング　課題

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function hello (a) {
+    let message1 = '';
+    if  (a === 0) {
+        message1 = '正解';
+    } else if (a === 1) {
+        message1 = '不正解';
+    } else {
+        message1 = '間違っています';
+    }
+    console.log(message1);
+    const hello = document.querySelector('#hello');
+    hello.textContent = message1;
+}
